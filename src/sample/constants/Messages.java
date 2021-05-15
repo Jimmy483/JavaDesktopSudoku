6495123a668e7375519e91c680417f1a0eb22aee
Jimmy Sherpa <jimmy483>
2021-05-15 21:40:42 +0545

    first commit

---
+++ src/sample/constants/Messages.java
@@ -0,0 +1,9 @@
+package sample.constants;
+
+public class Messages {
+
+    private static final String GAME_COMPLETE= "Congratulation, You have won the game";
+    private static final String ERROR="An Error has occurred";
+
+
+}
---
+++ src/sample/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package sample.problemdomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameData(SudokuGame game) throws IOException;
+    SudokuGame getGameData() throws IOException;
+}
