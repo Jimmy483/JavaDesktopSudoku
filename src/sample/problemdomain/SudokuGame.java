0cd53e8109c62aa56be2d3f9dd9f8dc202fa6cda
Jimmy Sherpa <jimmy483>
2021-05-20 17:41:49 +0545

    Hello stalker..completed UI

--- src/sample/problemdomain/SudokuGame.java
+++ src/sample/problemdomain/SudokuGame.java
@@ -1,5 +1,7 @@
 package sample.problemdomain;
 
+import sample.constants.GameState;
+
 public class SudokuGame {
     private final GameState gameState;
     private final int[][] gridState;
---
+++ src/sample/userinterface/IUserInterfaceContract.java
@@ -0,0 +1,19 @@
+package sample.userinterface;
+
+import sample.problemdomain.SudokuGame;
+
+public interface IUserInterfaceContract {
+
+    interface EventListerner{
+        void onSudokuInput(int x,int y,int input);
+        void onDialogClick();
+    }
+
+    interface View{
+        void setListener(IUserInterfaceContract.EventListerner listener);
+        void updateSquare(int x,int y,int input);
+        void updateBoard(SudokuGame game);
+        void showDialog(String message);
+        void showError(String message);
+    }
+}
