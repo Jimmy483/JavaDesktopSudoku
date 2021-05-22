01d9e816f22dc63e93985a56cd5e2040c77569a4
Jimmy Sherpa <jimmy483>
2021-05-22 23:51:47 +0545

    Hello Stalker....Completed Control Logic

--- src/sample/constants/Messages.java
+++ src/sample/constants/Messages.java
@@ -2,8 +2,8 @@ package sample.constants;
 
 public class Messages {
 
-    private static final String GAME_COMPLETE= "Congratulation, You have won the game";
-    private static final String ERROR="An Error has occurred";
+    public static final String GAME_COMPLETE= "Congratulation, You have won the game";
+    public static final String ERROR="An Error has occurred";
 
 
 }
