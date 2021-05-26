1b7be1f33e1dd01c3a41afa0e04bfe07db45e0dc
Jimmy Sherpa <jimmy483>
2021-05-26 18:49:26 +0545

    Hello Stalker....Continue from SudokuSolver

---
+++ src/sample/computationlogic/SudokuUtilities.java
@@ -0,0 +1,30 @@
+package sample.computationlogic;
+
+import sample.problemdomain.SudokuGame;
+
+import java.lang.reflect.Array;
+
+public class SudokuUtilities {
+    public static void copySudokuArrayValues(int[][] oldArray, int[][] newArray){
+        for(int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++){
+            for(int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++){
+
+                newArray[xIndex][yIndex] = oldArray[xIndex][yIndex];
+            }
+        }
+    }
+
+    public static  int[][] copyToNewArray(int[][] oldArray){
+        int[][] newArray = new int[SudokuGame.GRID_BOUNDARY][SudokuGame.GRID_BOUNDARY];
+
+        for(int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++){
+            for(int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++){
+
+                newArray[xIndex][yIndex] = oldArray[xIndex][yIndex];
+            }
+        }
+
+        return newArray;
+
+    }
+}
